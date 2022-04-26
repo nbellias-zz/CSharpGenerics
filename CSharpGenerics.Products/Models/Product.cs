a8e7eb291e7a8c695ab86c367ea3713194529809
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2022-04-26 21:02:50 +0300

    Products Books added

---
+++ CSharpGenerics.Products/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGenerics.Products.Models
+{
+    internal class Product
+    {
+        public Guid Id { get; set; }
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string LOT { get; set; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Product(string sku, string name, string lot)
+        {
+            Id = Guid.NewGuid();
+            SKU = sku;
+            Name = name;
+            LOT = lot;
+        }
+
+    }
+}
--- CSharpGenerics.Products/Program.cs
+++ CSharpGenerics.Products/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpGenerics.Products.Models;
+
+Console.WriteLine("CSharpGenerics program\n");
+
+Console.WriteLine(new Book("123-456-7890", "Elsevier", "890567", "Technical Book on Libs", "2022-01012T12:23:34Z"));
