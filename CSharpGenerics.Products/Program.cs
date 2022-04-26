fcf4b29f29924251eb5c5186cb4cbd1ee7bd86ab
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2022-04-26 23:29:04 +0300

    Some comments in Programs.cs

--- CSharpGenerics.Products/Program.cs
+++ CSharpGenerics.Products/Program.cs
@@ -3,6 +3,7 @@ using CSharpGenerics.Products.Models;
 
 Console.WriteLine("CSharpGenerics program\n");
 
+// Note the new ways for instantiating objects
 Book book = new("123-456-7890", "Elsevier", "890567", "Technical Book on Libs", "2022-01012T12:23:34Z");
 SmallAppliance _hairDryer = new SmallAppliance { Name = "Hair Dryer", Manufacturer = "Braun", Make = "Dry 230", SerNo = "123", LOT = "2019-10-08T12:34:20Z", SKU = "52-0897" };
 
